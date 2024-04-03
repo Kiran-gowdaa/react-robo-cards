bcc6d1a1117d9b8ec3e87759cdaa2e62f29e1d94
Kiran-gowdaa <chandrakiranrevanna@gmail.com>
2024-04-04 01:21:40 +0200

    error boundry for catching errors

--- robofriends/src/containers/App.js
+++ robofriends/src/containers/App.js
@@ -3,6 +3,7 @@ import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import "./App.css";
 import Scroll from "../components/Scroll";
+import ErrorBoundry from "../components/ErrorBoundry";
 
 class App extends React.Component {
   constructor() {
@@ -37,7 +38,9 @@ class App extends React.Component {
         <h1 className="f1">Robo Friends Cards</h1>
         <SearchBox searchChange={this.onSearchChange}></SearchBox>
         <Scroll>
-          <CardList robots={filteredRobots}></CardList>
+          <ErrorBoundry>
+            <CardList robots={filteredRobots}></CardList>
+          </ErrorBoundry>
         </Scroll>
       </div>
     );
